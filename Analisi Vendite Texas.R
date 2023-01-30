b82c42427c40558d55aa373b65726e4fec290ae6
Gabriele Naretto <narettogabriele@gmail.com>
2023-01-30 16:40:32 +0100

    primi plot

--- Analisi Vendite Texas.R
+++ Analisi Vendite Texas.R
@@ -12,4 +12,25 @@ attach(texas_price)
 #costruiamo la distribuzione di frequenza per la variabile città
 ni <- table(city)
 fi <- table(city)/length(city)
-Ni  
+
+#distribuzione di frequenza assoluta e relativa per citta
+distr_freq_ass_city = cbind(ni,fi)
+
+#diamo un occhiata allre frequenze relative doppie
+#distribuzione di frequenza assoluta per citta e anno
+distr_freq_ass_city_mese = table(city,year)
+
+#distribuzione di frequenza relativa per citta e anno
+distr_freq_rel_city_anno = table(city,year)/length(city)
+
+
+#Calcoliamo indici di posizione
+summary(sales)
+
+plot(density(sales))
+
+ggplot()+
+  geom_density(aes(x=sales,),col="black",fill="lightblue")
+
+  
+  
